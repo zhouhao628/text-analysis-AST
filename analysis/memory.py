974c89465ed86c074aee23cfbfdde62d4b8bdeda
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2021-10-06 07:37:46 +0000

    Bump version to 0.20.0

--- analysis/memory.py
+++ analysis/memory.py
@@ -8,4 +8,10 @@ import sys
 import time
 
 from analysis import utils
-from east.asts i
+from east.asts import base
+
+
+def memory_usage():
+    # return the memory usage in MB
+    process = psutil.Process(os.getpid())
+    mem = process.get_memory_info()[0] / f
