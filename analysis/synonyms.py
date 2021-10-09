02c6e948ba518a791a6e1f8a9fb20ae000b4296c
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2021-10-09 12:15:29 +0000

    Coq 8.5~beta2, repo released, mode clean.

--- analysis/synonyms.py
+++ analysis/synonyms.py
@@ -6,4 +6,11 @@ import sys
 from east.synonyms import synonyms
 
 def main(args):
-    opts, args = getop
+    opts, args = getopt.getopt(args, "")
+
+    path = args[0]
+
+    synonimizer = synonyms.SynonymExtractor(path)
+    print "Prepared synonimizer\n"
+
+    synonym_dicts = synonimizer.get_s
