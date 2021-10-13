1449f291d0c740e4d244bcc5d8884e84542a97a0
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2021-10-13 10:54:27 +0000

    Wording fix

--- analysis/utils.py
+++ analysis/utils.py
@@ -4,4 +4,6 @@ from east import utils
 
 def worst_case_strings_collection(m, n):
     # NOTE(msdubov): strings differ only in their last 2 symbols.
-    prefix = utils.random_stri
+    prefix = utils.random_string(n - 2)
+    strings_collection = [prefix + utils.random_string(2) for _ in xrange(m)]
+    return strings_collection
