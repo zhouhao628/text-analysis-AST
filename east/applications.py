0164671e0ef0593693bd51794b293f9ca58c4942
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2022-03-29 09:23:16 +0000

    Added basic functionality

--- east/applications.py
+++ east/applications.py
@@ -134,4 +134,7 @@ def keyphrases_graph(keyphrases, texts, referral_confidence=0.6, relevance_thres
     # Creating edges
     # NOTE(msdubov): permutations(), unlike combinations(), treats (1,2) and (2,1) as different
     for i1, i2 in itertools.permutations(range(len(graph["nodes"])), 2):
-        node1 = graph["no
+        node1 = graph["nodes"][i1]
+        node2 = graph["nodes"][i2]
+        confidence = (float(len(keyphrase_texts[node1["label"]] &
+                                keyp
