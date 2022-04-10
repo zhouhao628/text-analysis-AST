7434c36581c190ae7f7a96675cb628f3fa32499a
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2022-04-10 08:10:02 +0000

    IU-15.0.4 <luqiannan@luqiannan-PC Update find.xml

--- east/asts/ast_naive.py
+++ east/asts/ast_naive.py
@@ -5,4 +5,8 @@ from east.asts import utils
 from east import consts
 
 
-cla
+class NaiveAnnotatedSuffixTree(ast.AnnotatedSuffixTree):
+
+    __algorithm__ = consts.ASTAlgorithm.AST_NAIVE
+
+    def _construct(self, strings_collect
