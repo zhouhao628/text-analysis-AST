ff289b614fe795d35b80aeee13e5fb190df950c2
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2022-04-29 20:29:24 +0000

    Clean up whitespace.

--- east/asts/base.py
+++ east/asts/base.py
@@ -6,3 +6,10 @@ import inspect
 from east import consts
 from east import exceptions
 from east import utils
+
+class AST(object):
+    __metaclass__ = abc.ABCMeta
+
+    @staticmethod
+    def get_ast(strings_collection, ast_algorithm="easa"):
+        for ast_cls in utils.iters
