d3f889125f476ec49c9927cb5623e5d2de70703b
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2022-04-30 16:34:39 +0000

    Content-Editor changes

--- east/asts/base.py
+++ east/asts/base.py
@@ -12,4 +12,7 @@ class AST(object):
 
     @staticmethod
     def get_ast(strings_collection, ast_algorithm="easa"):
-        for ast_cls in utils.iters
+        for ast_cls in utils.itersubclasses(AST):
+            if not inspect.isabstract(ast_cls) and ast_algorithm == ast_cls.__algorithm__:
+                return ast_cls(strings_collection)
+    
