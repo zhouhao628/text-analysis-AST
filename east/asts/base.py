45a74b77bd56c94af38982ae2627a90fb74019b4
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2022-04-30 21:52:49 +0000

    Update android_bankbot.txt

--- east/asts/base.py
+++ east/asts/base.py
@@ -15,4 +15,8 @@ class AST(object):
         for ast_cls in utils.itersubclasses(AST):
             if not inspect.isabstract(ast_cls) and ast_algorithm == ast_cls.__algorithm__:
                 return ast_cls(strings_collection)
-    
+        raise exceptions.NoSuchASTAlgorithm(name=ast_algorithm)
+
+    def __init__(self, strings_collection):
+        if not strings_collection:
+            raise except
