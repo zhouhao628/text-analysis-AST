1d25601dc23879b232ee376692ec496bbdda53a2
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2022-05-09 14:47:43 +0000

    unused file

--- east/asts/easa.py
+++ east/asts/easa.py
@@ -218,3 +218,178 @@ class EnhancedAnnotatedSuffixArray(base.AST):
                         p += 1
                         k += 1          
             else: 
+                SA[k] = j
+                p += 1
+                if p == n0:
+                    k += 1
+                    while t < n02:
+                        SA[k] = (SA12[t] * 3) + 1 if SA12[t] < n0 else ((SA12[t] - n0) * 3) + 2
+                        t += 1
+                        k += 1
+            k += 1
+
+    def _radixpass(self, a, b, r, n, alpha):
+        c = {}
+        for letter in alpha:
+            c[letter] = 0
+        for i in xrange(n):
+            c[r[a[i]]] += 1
+
+        total = 0
+        for letter in alpha:
+            freq, c[letter] = c[letter], total
+            total += freq
+        for i in xrange(n):
+            b[c[r[a[i]]]] = a[i]
+            c[r[a[i]]] += 1
+
+        return b
+
+    def _compute_lcptab(self, string, suftab):
+        """Computes the LCP array in O(n) based on the input string & its suffix array.
+
+        Kasai et al. (2001).
+        """
+        n = len(suftab)
+        rank = [0] * n
+        for i in xrange(n):
+            rank[suftab[i]] = i
+        lcptab = np.zeros(n, dtype=np.int)
+        h = 0
+        for i in xrange(n):
+            if rank[i] >= 1:
+                j = suftab[rank[i] - 1]
+                while string[i + h] == string[j + h]:
+                    h += 1
+                lcptab[rank[i]] = h
+                if h > 0:
+                    h -= 1
+        return lcptab
+
+    def _compute_childtab(self, lcptab):
+        """Computes the child 'up' and 'down' arrays in O(n) based on the LCP table.
+
+        Abouelhoda et al. (2004).
+        """
+        last_index = -1
+        stack = [0]
+        n = len(lcptab)
+        childtab_up = np.zeros(n, dtype=np.int)  # Zeros / -1 ?
+        childtab_down = np.zeros(n, dtype=np.int)
+        for i in xrange(n):
+            while lcptab[i] < lcptab[stack[-1]]:
+                last_index = stack.pop()
+                if lcptab[i] <= lcptab[stack[-1]] and lcptab[stack[-1]] != lcptab[last_index]:
+                    childtab_down[stack[-1]] = last_index
+            if last_index != -1:
+                childtab_up[i] = last_index
+                last_index = -1
+            stack.append(i)
+        return childtab_up, childtab_down
+
+    def _compute_childtab_next_l_index(self, lcptab):
+        """Computes the child 'next l index' array in O(n) based on the LCP table.
+
+        Abouelhoda et al. (2004).
+        """
+        stack = [0]
+        n = len(lcptab)
+        childtab_next_l_index = np.zeros(n, dtype=np.int)  # Zeros / -1 ?
+        for i in xrange(n):
+            while lcptab[i] < lcptab[stack[-1]]:
+                stack.pop()
+            if lcptab[i] == lcptab[stack[-1]]:
+                last_index = stack.pop()
+                childtab_next_l_index[last_index] = i
+            stack.append(i)
+        return childtab_next_l_index
+
+    def _compute_anntab(self, suftab, lcptab):
+        """Computes the annotations array in O(n) by "traversing" the suffix array.
+
+        Based on ideas from Abouelhoda et al. (2004) and Dubov & Chernyak (2013).
+        """
+        n = len(suftab)
+        anntab = np.zeros(n, dtype=np.int)  # Zeros / -1 ?
+
+        def process_node(node):
+            # NOTE(msdubov): Assumes that child l-[i..j] lcp intervals come in the ascending
+            #                order (by i). This allows to handle the leafs properly.
+            i = node[1]
+            for child_node in node[3]:
+                if i < child_node[1]:
+                    anntab[self._interval_index(node)] += child_node[1] - i
+                anntab[self._interval_index(node)] += anntab[self._interval_index(child_node)]
+                i = child_node[2] + 1
+            if i <= node[2]:
+                anntab[self._interval_index(node)] += node[2] - i + 1
+
+        self.traverse_depth_first_post_order(process_node)
+        # NOTE(msdubov): Removing the "degenerate" 1st-level leafs
+        #                with the auxiliary symbol in the arc.
+        anntab[0] -= len(self.strings_collection)
+
+        return anntab
+
+    def _interval_index(self, lcp_interval):
+        """Maps an lcp interval to an index in [0..n-1].
+
+        :param lcp_interval: <l, i, j>.
+        """
+        return utils.index(self.lcptab, lcp_interval[0], lcp_interval[1])
+
+    def _annotation(self, lcp_interval):
+        if self._is_leaf(lcp_interval):
+            return 1
+        else:
+            return self.anntab[self._interval_index(lcp_interval)]
+
+    def _is_leaf(self, lcp_interval):
+        return lcp_interval[1] == lcp_interval[2]
+
+    def _lcp_value(self, i, j):
+        n = len(self.suftab)
+        if (i == 0 or i == n - 1) and j == n - 1:
+            return 0  # TODO: Verify the correctness of this step.
+        elif i < self.childtab_up[j + 1] <= j:
+            return self.lcptab[self.childtab_up[j + 1]]
+        else:
+            return self.lcptab[self.childtab_down[i]]
+
+    def _get_child_intervals(self, i, j):
+        if i == j:
+            return []
+        n = len(self.suftab)
+        l = self._lcp_value(i, j)
+        intervals = []
+        if i == 0 and j == n - 1:
+            i1 = 0  # TODO: Verify the correctness of this step.
+        else:
+            if i < self.childtab_up[j + 1]:
+                i1 = self.childtab_up[j + 1]
+            else:
+                i1 = self.childtab_down[i]
+            intervals.append((self._lcp_value(i, i1 - 1), i, i1 - 1, self.string[self.suftab[i] + l]))
+        while self.childtab_next_l_index[i1] != 0:
+            i2 = self.childtab_next_l_index[i1]
+            intervals.append((self._lcp_value(i1, i2 - 1), i1, i2 - 1, self.string[self.suftab[i1] + l]))
+            i1 = i2
+        intervals.append((self._lcp_value(i1, j), i1, j, self.string[self.suftab[i1] + l]))
+        return intervals
+
+    def _get_child_interval(self, i, j, char):
+        if i == j:
+            return None
+        n = len(self.suftab)
+        l = self._lcp_value(i, j)
+        if i == 0 and j == n - 1:
+            i1 = 0
+        else:
+            if i < self.childtab_up[j + 1]:
+                i1 = self.childtab_up[j + 1]
+            else:
+                i1 = self.childtab_down[i]
+            if self.string[self.suftab[i] + l] == char:
+                return (self._lcp_value(i, i1 - 1), i, i1 - 1, self.string[self.suftab[i] + l])
+        while self.childtab_next_l_index[i1] != 0:
+            i2 = self.childtab_next_l_index[i1]
