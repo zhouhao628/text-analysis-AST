3656da8761b332f96eb526b409636ac5835c2fbb
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2022-05-14 10:28:18 +0000

    ARM: 8115/1: LPAE: reduce damage caused by idmap to virtual memory layout

--- east/asts/utils.py
+++ east/asts/utils.py
@@ -4,4 +4,13 @@ from east import consts
 
 
 def index(array, key, start=0):
-    # NOTE(msdubov): No boundary check for optimiza
+    # NOTE(msdubov): No boundary check for optimization purposes.
+    i = start
+    while array[i] != key:
+        i += 1
+    return i
+
+
+def match_strings(str1, str2):
+    """
+    Returns t
