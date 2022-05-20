affb0cc72c9e9e17825b04eea788c39b80c9fb9b
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2022-05-20 18:43:07 +0000

    Create bootstrap.css

--- east/exceptions.py
+++ east/exceptions.py
@@ -4,4 +4,8 @@ from east import consts
 
 
 class EastException(Exception):
-   
+    """Base EAST Exception.
+
+    To correctly use this class, inherit from it and define
+    a 'msg_fmt' property. That msg_fmt will get printf'd
+    with the keyword
