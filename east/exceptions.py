dc2fdcb50e26a2618558f3c505e22debbe00f5a0
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2022-05-21 08:40:28 +0000

    lots of stuff

--- east/exceptions.py
+++ east/exceptions.py
@@ -8,4 +8,8 @@ class EastException(Exception):
 
     To correctly use this class, inherit from it and define
     a 'msg_fmt' property. That msg_fmt will get printf'd
-    with the keyword
+    with the keyword arguments provided to the constructor.
+    """
+    msg_fmt = "An unknown exception occurred."
+
+    def __init__(self, message=None, **kwargs)
