1aac98b2f84cad32ba61e420e918fa0627d6b073
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2022-06-12 21:02:09 +0000

    Corrections to README

--- east/logging.py
+++ east/logging.py
@@ -1,1 +1,10 @@
-# -*- 
+# -*- coding: utf-8 -*
+
+import sys
+
+from east import utils
+
+
+def progress(message, step, total):
+    if not utils.output_is_redirected():
+        sys.stdout.write("\r%s: %i/%
