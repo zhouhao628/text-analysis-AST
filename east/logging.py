3dc8ace9e6b0d3dc3c98efba9cfc1e89b5e861b6
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2022-06-14 13:24:59 +0000

    Remove unused type

--- east/logging.py
+++ east/logging.py
@@ -7,4 +7,10 @@ from east import utils
 
 def progress(message, step, total):
     if not utils.output_is_redirected():
-        sys.stdout.write("\r%s: %i/%
+        sys.stdout.write("\r%s: %i/%i" % (message, step, total))
+        sys.stdout.flush()
+
+
+def clear():
+    if not utils.output_is_redirected():
+        sys.stdout.write("\r" + "
