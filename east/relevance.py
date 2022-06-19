08a5f4f5bd475bebe87f4a5e09ca517c9a49e6f3
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2022-06-19 16:20:07 +0000

    Forward port zendframework/zf2#3554

--- east/relevance.py
+++ east/relevance.py
@@ -4,4 +4,13 @@ from collections import defaultdict
 import math
 import sys
 
-from nltk.stem import sn
+from nltk.stem import snowball
+import numpy as np
+
+from east.asts import base
+from east import consts
+from east import logging
+from east import utils
+
+
+class RelevanceM
