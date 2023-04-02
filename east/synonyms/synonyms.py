0ef755486130c6767dd9b8c4f6f4a54e3ba142ac
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2023-04-02 22:54:17 +0000

    added some comments.

--- east/synonyms/synonyms.py
+++ east/synonyms/synonyms.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*
 
-im
+import codecs
+import collections
+import itertools
+import math
+import os
+import subprocess
+from xml.dom import minidom
+
+
+from east import consts
+from east
