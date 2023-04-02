494f76cf202c5ae62515c52eaa73a49b89c4cc37
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2023-04-02 23:03:36 +0000

    appveyor

--- east/synonyms/synonyms.py
+++ east/synonyms/synonyms.py
@@ -10,4 +10,12 @@ from xml.dom import minidom
 
 
 from east import consts
-from east
+from east import exceptions
+from east import utils as common_utils
+from east.synonyms import utils
+
+
+class SynonymExtractor(object):
+
+    def __init__(self, input_path):
+       
