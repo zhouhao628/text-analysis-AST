eb423c9f684020333f0d307cb9647f9abefb492f
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2023-06-23 22:23:56 +0000

    hoi

--- east/synonyms/utils.py
+++ east/synonyms/utils.py
@@ -8,4 +8,8 @@ from east import consts
 def determine_operating_system():
     system = platform.system()
     bits = platform.architecture()[0]
-    if s
+    if system == "Windows":
+        return consts.OperatingSystem.WINDOWS
+    elif system == "Linux":
+        if bits.startswith("64"):
+            return consts.Ope
