95f6f97a8ca3077d3d3eb1612af60ccc51da1930
zhouhao628 <33346158+zhouhao628@users.noreply.github.com>
2023-06-23 17:53:30 +0000

    Fix too long line

--- east/synonyms/utils.py
+++ east/synonyms/utils.py
@@ -1,1 +1,11 @@
-# -*
+# -*- coding: utf-8 -*
+
+import platform
+
+from east import consts
+
+
+def determine_operating_system():
+    system = platform.system()
+    bits = platform.architecture()[0]
+    if s
